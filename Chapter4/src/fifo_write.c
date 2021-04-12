f35505f37644a9e5511069b002e70ec1ce25ab58
gaishishenlun <1138092572@qq.com>
2021-04-12 21:23:43 +0800

    0412

---
+++ Chapter4/src/fifo_write.c
@@ -0,0 +1,41 @@
+/* fifo_write.c */
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+#define MYFIFO					"/tmp/myfifo"			// 有名管道文件名
+#define MAX_BUFFER_SIZE	PIPE_BUF	// 定义在limits.h
+
+
+int main(int argc, char *argv[]) {
+
+	int fd;
+	char buff[MAX_BUFFER_SIZE];
+	int nwrite;
+
+	if (argc <= 1) {
+		printf("Usage: ./fifo_write string\n");
+		exit(1);
+	}
+
+	sscanf(argv[1], "%s", buff);
+	// 以只写阻塞方式打开FIFO管道
+	fd = open(MYFIFO, O_WRONLY);
+
+	if (-1 == fd) {
+		printf("Open fifo file error\n");
+		exit(1);
+	}
+
+	// 向管道中写入字符串
+	if ((nwrite = write(fd, buff, MAX_BUFFER_SIZE)) > 0) {
+		printf("Write '%s' to FIFO\n", buff);
+	}
+
+	close(fd);
+	exit(0);
+}
---
+++ Chapter4/src/pipe_select.c
@@ -0,0 +1,118 @@
+/* pipe_select.c */
+#include<fcntl.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<errno.h>
+#include<sys/select.h>
+
+#define FIFO1 "in1"
+#define FIFO2 "in2"
+#define MAX_BUFFER_SIZE		1024	// 缓冲区大小
+#define IN_FILES					3			// 多路复用输入文件数目
+#define TIME_DELAY				5		// 超时值秒数
+#define MAX(a, b) ((a > b)?(a):(b))
+
+
+int main(void) {
+	int fds[IN_FILES];
+	char buf[MAX_BUFFER_SIZE];
+	int i, res, real_read, maxfd;
+	struct timeval tv;
+	fd_set inset, tmp_inset;
+	fds[0] = 0;
+
+	// 创建两个有名管道
+	if (-1 == access(FIFO1, F_OK)) {
+		if ((mkfifo(FIFO1, 0666) < 0) && (errno != EEXIST)) {
+			printf("Cannot create fifo file\n");
+			exit(1);
+		}
+	}
+
+	if (-1 == access(FIFO2, F_OK)) {
+		if ((mkfifo(FIFO2, 0666) < 0) && (errno != EEXIST)) {
+			printf("Cannot create fifo file\n");
+			exit(1);
+		}
+	}
+
+	// 以只读非阻塞方式打开两个管道文件
+	if ((fds[1] = open(FIFO1, O_RDONLY|O_NONBLOCK)) < 0) {
+		printf("Open in1 error\n");
+		return 1;
+	}
+
+	if ((fds[2] = open(FIFO2, O_RDONLY|O_NONBLOCK)) < 0) {
+		printf("Open in2 error\n");
+		return 1;
+	}
+
+	// 取出两个文件描述符中的较大者
+	maxfd = MAX(MAX(fds[0], fds[1]), fds[2]);
+	// 初始化读集inset，并在读文件描述符集中加入相应的描述集
+	FD_ZERO(&inset);
+
+	for (i = 0; i < IN_FILES; i++) {
+		FD_SET(fds[i], &inset);
+	}
+	FD_SET(0, &inset);
+
+	tv.tv_sec = TIME_DELAY;
+	tv.tv_usec = 0;
+
+	// 循环测试该文件描述符是否准备就绪，并调用select()函数对相关文件描述符做相应操作
+	while (FD_ISSET(fds[0], &inset) || FD_ISSET(fds[1], &inset) || FD_ISSET(fds[2], &inset)) {
+		// 文件描述符集的备份，以免每次都进行初始化
+		tmp_inset = inset;
+		res = select(maxfd + 1, &tmp_inset, NULL, NULL, &tv);
+		switch (res) {
+			case -1: {
+					printf("Select error\n");
+					return 1;
+				}
+				break;
+			case 0: {
+				printf("Time out\n");
+				return 1;
+				}
+				break;
+			default: {
+					for (i = 1; i < IN_FILES; i++) {
+
+						memset(buf, 0, MAX_BUFFER_SIZE);
+						real_read = read(fds[i], buf, MAX_BUFFER_SIZE);
+
+						if ( real_read < 0) {
+							if (errno != EAGAIN) {
+								printf("fds[%d] = %d errno!\n", i, fds[i]);
+								return 1;
+							}
+						}
+						else if (!real_read) {
+							close(fds[i]);
+							FD_CLR(fds[i], &inset);
+						}
+						else {
+							if (i == 0) {
+								if ((buf[0] == 'q') || (buf[0] == 'Q')) {
+								// 主程序终端控制
+									printf("Q退出\n");
+									return 1;
+								}
+							}
+							else {
+							// 显示管道输入字符串
+								buf[real_read] = '\0';
+								printf("%s", buf);
+							}
+						}// end of if
+					}// end of for
+				}
+				break;
+		}// end of switch
+	}// end of while
+	return 0;
+}
